39d7401b1351ba3f49135ab9e4d20282243b8b22
AkilaManu <akilamanujith@gmail.com>
2025-03-20 22:47:57 +0530

    first commit

---
+++ deliver-products/src/main/java/com/example/deliver_products/DeliverProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliver_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverProductsApplication.class, args);
+	}
+
+}
---
+++ deliver-products/src/test/java/com/example/deliver_products/DeliverProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliver_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
